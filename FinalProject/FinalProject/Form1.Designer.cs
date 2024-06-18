de82949fda28030af6765e3c4ed5bde141180468
ba77777 <126754849+ba77777@users.noreply.github.com>
2024-06-18 18:02:19 +0300

    fixed problems 2

--- FinalProject/FinalProject/Form1.Designer.cs
+++ FinalProject/FinalProject/Form1.Designer.cs
@@ -75,7 +75,7 @@ namespace FinalProject
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(90, 92);
             this.button1.TabIndex = 2;
-            this.button1.Text = "idk";
+            this.button1.Text = "Save";
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.btn_saveBoard);
             // 
